3d9b635f0b7f95cdb2691f71bc0d017f34a284e9
Janet I Kidane <janetisakidane@gmail.com>
2023-10-20 16:55:36 -0500

    Adding project files

---
+++ backend/src/test/java/edu/miu/cs545/biddingproject/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs545.biddingproject.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
