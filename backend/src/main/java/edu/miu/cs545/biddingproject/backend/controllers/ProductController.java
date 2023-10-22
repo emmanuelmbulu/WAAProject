870223611d202a0d76cc93342ebdcb715e21236c
Emmanuel Mbulu <emmanuel.mbulu@gmail.com>
2023-10-21 20:17:37 -0500

    Implementing get all products and get single product actions from the controller

--- backend/src/main/java/edu/miu/cs545/biddingproject/backend/controllers/ProductController.java
+++ backend/src/main/java/edu/miu/cs545/biddingproject/backend/controllers/ProductController.java
@@ -5,13 +5,11 @@ import edu.miu.cs545.biddingproject.backend.domains.Seller;
 import edu.miu.cs545.biddingproject.backend.queries.DataForNewProduct;
 import edu.miu.cs545.biddingproject.backend.services.ProductService;
 import edu.miu.cs545.biddingproject.backend.services.SellerService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("products")
@@ -26,6 +24,11 @@ public class ProductController {
         this.sellerService = sellerService;
     }
 
+    @GetMapping("")
+    public List<Product> getAllProducts() {
+        return service.getAllProducts();
+    }
+
     @PostMapping("")
     public ResponseEntity<?> createProduct(@RequestBody DataForNewProduct data) {
         Seller seller = sellerService.getOneById(data.getSellerId());
@@ -37,6 +40,13 @@ public class ProductController {
                 .isSavedWithRelease(data.isSavedWithRelease())
                 .build();
         product = service.save(product);
+        return ResponseEntity.ok(product);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getOneProduct(@PathVariable Long id) {
+        Product product = service.getOneById(id);
+        if(product == null) return ResponseEntity.notFound().build();
         return ResponseEntity.ok(product);
     }
 }
