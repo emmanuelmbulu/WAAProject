4482f0e99d592b5b7a9e916f7bca3b552b7a867a
Emmanuel Mbulu <emmanuel.mbulu@gmail.com>
2023-10-21 23:37:10 -0500

    Implementing get bids and highest bid for a product

--- backend/src/main/java/edu/miu/cs545/biddingproject/backend/controllers/ProductController.java
+++ backend/src/main/java/edu/miu/cs545/biddingproject/backend/controllers/ProductController.java
@@ -3,6 +3,7 @@ package edu.miu.cs545.biddingproject.backend.controllers;
 import edu.miu.cs545.biddingproject.backend.domains.Product;
 import edu.miu.cs545.biddingproject.backend.domains.Seller;
 import edu.miu.cs545.biddingproject.backend.queries.DataForNewProduct;
+import edu.miu.cs545.biddingproject.backend.services.BidService;
 import edu.miu.cs545.biddingproject.backend.services.ProductService;
 import edu.miu.cs545.biddingproject.backend.services.SellerService;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -17,11 +18,14 @@ public class ProductController {
 
     final private ProductService service;
     final private SellerService sellerService;
+    final private BidService bidService;
 
     public ProductController( @Qualifier("productServiceImpl") ProductService service,
-                              SellerService sellerService) {
+                              SellerService sellerService,
+                              BidService bidService) {
         this.service = service;
         this.sellerService = sellerService;
+        this.bidService = bidService;
     }
 
     @GetMapping("")
@@ -49,5 +53,26 @@ public class ProductController {
         Product product = service.getOneById(id);
         if(product == null) return ResponseEntity.notFound().build();
         return ResponseEntity.ok(product);
+    }
+
+    @GetMapping("/{id}/bids")
+    public ResponseEntity<?> getAllBidsForProduct(@PathVariable Long id) {
+        Product product = service.getOneById(id);
+        if(product == null) return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(bidService.getAllBidsByProduct(product));
+    }
+
+    @GetMapping("/{id}/bids")
+    public ResponseEntity<?> getTheLatestBidForProduct(@PathVariable Long id,
+                                                       @RequestParam(name = "filter") String filter) {
+        Product product = service.getOneById(id);
+        if(product == null) return ResponseEntity.notFound().build();
+
+        if(filter == null || filter.isEmpty()) return ResponseEntity.ok(bidService.getAllBidsByProduct(product));
+        if("latest".equalsIgnoreCase(filter)) {
+            return ResponseEntity.ok(bidService.getTheLatestBidByProduct(product));
+        }
+        return ResponseEntity.noContent().build();
     }
 }
